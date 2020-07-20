0564648fdbe6a8c6d3a84ec2aa095bf3eed38863
Ross Hutchens <rlhcxm@gmail.com>
2020-07-19 20:13:57 -0500

    review page setup

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -6,7 +6,7 @@ class Review extends Component {
 
   handleClick = (event) => {
     event.preventDefault();
-    // this.props.history.push('/home');
+    this.props.history.push('/');
   }
 
   render() {
@@ -14,6 +14,12 @@ class Review extends Component {
       <div>
         <form>
         <h1>Review your feedback</h1>
+        <ul>
+          <li>Feeling: {this.props.reduxState.feedbackReducer.feeling}</li>
+          <li>Understanding: {this.props.reduxState.feedbackReducer.understanding}</li>
+          <li>Support: {this.props.reduxState.feedbackReducer.support}</li>
+          <li>Comments: {this.props.reduxState.feedbackReducer.comments}</li>
+        </ul>
         <button onClick={this.handleClick}>Done</button>
         </form>
       </div>
